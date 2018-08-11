9d2590d2d103d33aaf051c9c32b2b8b8e2ccef9b
Adriano Lima <jink_adriano@hotmail.com>
2018-08-10 21:58:06 -0300

    Oitavo commit

--- src/main/java/com/adriano/cursomc/services/CategoriaService.java
+++ src/main/java/com/adriano/cursomc/services/CategoriaService.java
@@ -8,15 +8,19 @@ import org.springframework.stereotype.Service;
 import com.adriano.cursomc.domain.Categoria;
 import com.adriano.cursomc.repositories.CategoriaRepository;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) throws ObjectNotFoundException{
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
 
 }
